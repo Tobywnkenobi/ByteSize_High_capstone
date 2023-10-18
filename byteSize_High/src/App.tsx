8767f9098e618a4fac2d4c1bd2d03806103b8483
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-10-18 05:12:12 -0400

    please don't crash computer!

--- byteSize_High/src/App.tsx
+++ byteSize_High/src/App.tsx
@@ -1,8 +1,42 @@
-// import { useState } from 'react'
-import './index.css'
+// import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+
+// Components
+import Students from './components/Students.js';
+import Parents from './components/Parents.js';
+import Levels from './components/Levels.js';
 
 function App() {
-    return('')
+    // Example student data
+    const studentData = {
+        name: 'Raul',
+        age: 12,
+        grade: "4th grade",
+        level: 2
+    }
+
+    return (
+        <Router>
+            <div>
+                <header>
+                    <h1>Special Education Dashboard</h1>
+                    <p>Enter Lafayette Portal.</p>
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<div>
+                        <Students studentInfo={studentData} />
+                        <Parents />
+                        <Levels />
+                    </div>} />
+                </Routes>
+
+                <footer>
+                    <p>Developed with ❤️ for Lafayette</p>
+                </footer>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
